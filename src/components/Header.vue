96692e3632be516970f3fcf393a998763cc02595
Cristhian Caycedo <cacaycedo@gov.co>
2024-09-18 12:00:23 +0700

    Change code

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,7 @@ import {iconLogo} from "@/image";
   justify-content: space-between;
   background-color: #13cfa0;
   height: 50px;
-  padding: 0% 5%;
+  padding: 2% 5%;
   align-items: center;
   .logo-row {
     width: 50%;
@@ -23,6 +23,70 @@ import {iconLogo} from "@/image";
     display: flex;
     justify-content: end;
     width: 50%;
+
+    .menu {
+      display: flex;
+      flex-direction: row;
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+    }
+
+    .menu > li {
+      margin: 0 1rem;
+      overflow: hidden;
+    }
+
+    .menu-button-container {
+      display: none;
+      height: 100%;
+      width: 30px;
+      cursor: pointer;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+
+    #menu-toggle {
+      display: none;
+    }
+
+    .menu-button,
+    .menu-button::before,
+    .menu-button::after {
+      display: block;
+      background-color: #fff;
+      position: absolute;
+      height: 4px;
+      width: 30px;
+      transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
+      border-radius: 2px;
+    }
+
+    .menu-button::before {
+      content: '';
+      margin-top: -8px;
+    }
+
+    .menu-button::after {
+      content: '';
+      margin-top: 8px;
+    }
+
+    #menu-toggle:checked + .menu-button-container .menu-button::before {
+      margin-top: 0px;
+      transform: rotate(405deg);
+    }
+
+    #menu-toggle:checked + .menu-button-container .menu-button {
+      background: rgba(255, 255, 255, 0);
+    }
+
+    #menu-toggle:checked + .menu-button-container .menu-button::after {
+      margin-top: 0px;
+      transform: rotate(-405deg);
+    }
+
     .button {
       background: none;
       border-radius: 5px;
@@ -39,6 +103,46 @@ import {iconLogo} from "@/image";
         cursor: pointer;
       }
     }
+    @media screen and (max-width: 1200px) {  
+      .menu-button-container {
+        display: flex;
+      }
+      .menu {
+        position: absolute;
+        top: 0;
+        margin-top: 50px;
+        left: 0;
+        flex-direction: column;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+      }
+      #menu-toggle ~ .menu li {
+        height: 0;
+        margin: 0;
+        padding: 0;
+        border: 0;
+        transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+      }
+      #menu-toggle:checked ~ .menu li {
+        border: 1px solid #fff;
+        height: 3.5rem;
+        padding: 0.5em;
+        transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+      }
+      .menu > li {
+        display: flex;
+        justify-content: center;
+        margin: 0;
+        padding: 0.5em 0;
+        width: 100%;
+        color: white;
+        background-color: #13cfa0;
+      }
+      .menu > li:not(:last-child) {
+        border-bottom: 1px solid #13cfa0;
+      }
+    }
   }
 }
 </style>
@@ -49,9 +153,15 @@ import {iconLogo} from "@/image";
       <span class="text">Hieu Tran</span>
     </div>
     <div class="button-row">
-      <button type="button" class="button">Indie Game</button>
-      <button type="button" class="button">Entry Game</button>
-      <button type="button" class="button">Community</button>
+      <input id="menu-toggle" type="checkbox" />
+        <label class="menu-button-container" for="menu-toggle">
+        <div class="menu-button"></div>
+      </label>
+      <ul class="menu">
+        <li><button type="button" class="button">Indie Game</button></li>
+        <li><button type="button" class="button">Entry Game</button></li>
+        <li><button type="button" class="button">Community</button></li>
+      </ul> 
     </div>
   </div>
 </template>
